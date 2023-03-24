a032e57fd5914bab4efb3dfbd007abf4824d764f
GlouGloups <cboodnee@gmail.com>
2023-03-24 19:36:50 +0100

    Add components

--- ChrisM/Form1.Designer.cs
+++ ChrisM/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.gbxbuttonsbottom = new System.Windows.Forms.GroupBox();
+            this.btnSearchFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxbuttonsbottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(457, 19);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(71, 66);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "button1";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // gbxbuttonsbottom
+            // 
+            this.gbxbuttonsbottom.Controls.Add(this.trackBar1);
+            this.gbxbuttonsbottom.Controls.Add(this.button4);
+            this.gbxbuttonsbottom.Controls.Add(this.button3);
+            this.gbxbuttonsbottom.Controls.Add(this.button2);
+            this.gbxbuttonsbottom.Controls.Add(this.button1);
+            this.gbxbuttonsbottom.Controls.Add(this.btnSearchFile);
+            this.gbxbuttonsbottom.Controls.Add(this.btnPlay);
+            this.gbxbuttonsbottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gbxbuttonsbottom.Location = new System.Drawing.Point(0, 361);
+            this.gbxbuttonsbottom.Name = "gbxbuttonsbottom";
+            this.gbxbuttonsbottom.Size = new System.Drawing.Size(984, 100);
+            this.gbxbuttonsbottom.TabIndex = 1;
+            this.gbxbuttonsbottom.TabStop = false;
+            // 
+            // btnSearchFile
+            // 
+            this.btnSearchFile.Location = new System.Drawing.Point(900, 19);
+            this.btnSearchFile.Name = "btnSearchFile";
+            this.btnSearchFile.Size = new System.Drawing.Size(71, 66);
+            this.btnSearchFile.TabIndex = 1;
+            this.btnSearchFile.Text = "button1";
+            this.btnSearchFile.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(303, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(611, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 66);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(31, 40);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(303, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 322);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 277);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gbxbuttonsbottom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbxbuttonsbottom.ResumeLayout(false);
+            this.gbxbuttonsbottom.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.GroupBox gbxbuttonsbottom;
+        private System.Windows.Forms.Button btnSearchFile;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
