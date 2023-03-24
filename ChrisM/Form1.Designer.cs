f7941f9c12094204a40f43b9de18ecd7236960df
GlouGloups <cboodnee@gmail.com>
2023-03-24 19:46:29 +0100

    Add design names to components

--- ChrisM/Form1.Designer.cs
+++ ChrisM/Form1.Designer.cs
@@ -31,16 +31,16 @@
             this.btnPlay = new System.Windows.Forms.Button();
             this.gbxbuttonsbottom = new System.Windows.Forms.GroupBox();
             this.btnSearchFile = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnGoToFirstMusic = new System.Windows.Forms.Button();
+            this.btnGoToLastMusic = new System.Windows.Forms.Button();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbxTitleMusics = new System.Windows.Forms.ListBox();
+            this.lblTitlesMusics = new System.Windows.Forms.Label();
             this.gbxbuttonsbottom.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,11 +55,11 @@
             // 
             // gbxbuttonsbottom
             // 
-            this.gbxbuttonsbottom.Controls.Add(this.trackBar1);
-            this.gbxbuttonsbottom.Controls.Add(this.button4);
-            this.gbxbuttonsbottom.Controls.Add(this.button3);
-            this.gbxbuttonsbottom.Controls.Add(this.button2);
-            this.gbxbuttonsbottom.Controls.Add(this.button1);
+            this.gbxbuttonsbottom.Controls.Add(this.tbVolume);
+            this.gbxbuttonsbottom.Controls.Add(this.btnGoToLastMusic);
+            this.gbxbuttonsbottom.Controls.Add(this.btnGoToFirstMusic);
+            this.gbxbuttonsbottom.Controls.Add(this.btnPrevious);
+            this.gbxbuttonsbottom.Controls.Add(this.btnNext);
             this.gbxbuttonsbottom.Controls.Add(this.btnSearchFile);
             this.gbxbuttonsbottom.Controls.Add(this.btnPlay);
             this.gbxbuttonsbottom.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -78,49 +78,49 @@
             this.btnSearchFile.Text = "button1";
             this.btnSearchFile.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnNext
             // 
-            this.button1.Location = new System.Drawing.Point(534, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 66);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnNext.Location = new System.Drawing.Point(534, 19);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(71, 66);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "button1";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnPrevious
             // 
-            this.button2.Location = new System.Drawing.Point(380, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(71, 66);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnPrevious.Location = new System.Drawing.Point(380, 19);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(71, 66);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "button1";
+            this.btnPrevious.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnGoToFirstMusic
             // 
-            this.button3.Location = new System.Drawing.Point(303, 19);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(71, 66);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button1";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnGoToFirstMusic.Location = new System.Drawing.Point(303, 19);
+            this.btnGoToFirstMusic.Name = "btnGoToFirstMusic";
+            this.btnGoToFirstMusic.Size = new System.Drawing.Size(71, 66);
+            this.btnGoToFirstMusic.TabIndex = 4;
+            this.btnGoToFirstMusic.Text = "button1";
+            this.btnGoToFirstMusic.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnGoToLastMusic
             // 
-            this.button4.Location = new System.Drawing.Point(611, 19);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(71, 66);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button1";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnGoToLastMusic.Location = new System.Drawing.Point(611, 19);
+            this.btnGoToLastMusic.Name = "btnGoToLastMusic";
+            this.btnGoToLastMusic.Size = new System.Drawing.Size(71, 66);
+            this.btnGoToLastMusic.TabIndex = 5;
+            this.btnGoToLastMusic.Text = "button1";
+            this.btnGoToLastMusic.UseVisualStyleBackColor = true;
             // 
-            // trackBar1
+            // tbVolume
             // 
-            this.trackBar1.Location = new System.Drawing.Point(31, 40);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.TabIndex = 2;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.tbVolume.Location = new System.Drawing.Point(16, 40);
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(200, 45);
+            this.tbVolume.TabIndex = 2;
+            this.tbVolume.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
             // pictureBox1
             // 
@@ -130,24 +130,26 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // listBox1
+            // lbxTitleMusics
             // 
-            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.listBox1.ForeColor = System.Drawing.Color.White;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(15, 64);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(201, 277);
-            this.listBox1.TabIndex = 3;
+            this.lbxTitleMusics.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.lbxTitleMusics.ForeColor = System.Drawing.Color.White;
+            this.lbxTitleMusics.FormattingEnabled = true;
+            this.lbxTitleMusics.Location = new System.Drawing.Point(15, 64);
+            this.lbxTitleMusics.Name = "lbxTitleMusics";
+            this.lbxTitleMusics.Size = new System.Drawing.Size(201, 277);
+            this.lbxTitleMusics.TabIndex = 3;
             // 
-            // label1
+            // lblTitlesMusics
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.lblTitlesMusics.AutoSize = true;
+            this.lblTitlesMusics.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitlesMusics.ForeColor = System.Drawing.Color.White;
+            this.lblTitlesMusics.Location = new System.Drawing.Point(12, 25);
+            this.lblTitlesMusics.Name = "lblTitlesMusics";
+            this.lblTitlesMusics.Size = new System.Drawing.Size(54, 24);
+            this.lblTitlesMusics.TabIndex = 4;
+            this.lblTitlesMusics.Text = "Titles";
             // 
             // Form1
             // 
@@ -155,15 +157,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblTitlesMusics);
+            this.Controls.Add(this.lbxTitleMusics);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.gbxbuttonsbottom);
             this.Name = "Form1";
             this.Text = "Form1";
             this.gbxbuttonsbottom.ResumeLayout(false);
             this.gbxbuttonsbottom.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -175,14 +177,14 @@
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.GroupBox gbxbuttonsbottom;
         private System.Windows.Forms.Button btnSearchFile;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.Button btnGoToLastMusic;
+        private System.Windows.Forms.Button btnGoToFirstMusic;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxTitleMusics;
+        private System.Windows.Forms.Label lblTitlesMusics;
     }
 }
 
